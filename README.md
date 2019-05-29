608f61ae77228a506ff4aff9e29a5d3c95aec349
cengel <cengel248@yahoo.com>
2019-05-29 10:15:17 -0700

    Major update: Add USA Street Address geocoder

--- README.md
+++ README.md
@@ -63,55 +63,114 @@ The ArcGIS REST geocoding service v10.0 and later takes addresses in [Single Lin
 
 Furthermore, there are two ways to send the addresses to the geocoding service: individually or as batch of several. Individual requests are more time consuming. For example, geocoding 1000 addresses takes over 2 minutes as single requests vs. 15 seconds as batch request. Batch geocoding is slightly faster when the address components are stored in separate fields (Multi Line). However, if there is an error in your batch, all the addresses in that batch that already have been geocoded will be dropped. **The maximum number of addresses that can be geocoded in a single batch request on the Stanford geocode server is 1000**.
 
+Lastly, there are two different geocoding services available for the US on Stanford's geolocator: _USA_Composite_ and _USA_StreetAddress_. I have found that for Single Line mode USA Composite provides (supposedly more exact) "PointAddress" results and USA StreetAddress returns (expectedly) "StreetAddress" results, while in Multiline (Batch) mode USA Composite returns "Locality", but USA StreetAddress provides (also expectedly, but possibly more exact) "StreetAddress" results.
+
 ## R geocode functions
 
 Here I provide two very simple, **by no means foolproof** R functions to do this. 
 
+First load the functions.
+
+    # source the R code
+    source("https://raw.githubusercontent.com/cengel/ArcGIS_geocoding/master/SUL_gcFunctions.R")
+
+
 ### Single Line
+
 `geocodeSL` takes one address at a time in Singleline format. Each address-string is sent as a single request to the geocode server.
 
+**Arguments:**
+
+address - one address at a time as one string (SingleLine)  
+token - token  
+geocoder - which geocoding service to use: USA_Comp (USA Composite) or USA_Str (USA StreetAddress), default is USA_Comp  
+postal - allow to return Postal codes if a full street address match cannot be found, default is TRUE  
+
+**The function returns:**
+
+lon, lat -    The primary x/y coordinates of the address returned by the geocoding service in WGS84  
+score -       The accuracy of the address match between 0 and 100.  
+status -      Whether a batch geocode request results in a match (M), tie (T), or unmatch (U)  
+matchAddr -   Complete address returned for the geocode request.  
+side -        The side of the street where an address resides relative to the direction 
+               of feature digitization  
+addressType - The match level for a geocode request. "PointAddress" is typically the 
+               most spatially accurate match level. "StreetAddress" differs from PointAddress 
+               because the house number is interpolated from a range of numbers. "StreetName" is similar,
+               but without the house number.   
+
 To use it do this.
 
-    # make up some addresses:
-    adr <- c('450 Serra Mall, Stanford, CA, 94305',
-              '1600 Amphitheatre Pkwy, Mountain View, CA 94043',
-              '1355 Market Street Suite 900, San Francisco, CA 94103')
+```R
+# make up some addresses:
+adr <- c('450 Serra Mall, Stanford, CA, 94305',
+         '1600 Amphitheatre Pkwy, Mountain View, CA 94043',
+         '1355 Market Street Suite 900, San Francisco, CA 94103')
 
-    # source the R code
-    source("https://raw.githubusercontent.com/cengel/ArcGIS_geocoding/master/SUL_gcFunctions.R")
+# set your token
+myToken <- "YOUR TOKEN HERE"
 
-    # set your token
-    myToken <- "YOUR TOKEN HERE"
+# geocode with US Composite
+do.call("rbind", lapply(adr, function(x) geocodeSL(x, myToken)))
 
-    # geocode with
-    do.call("rbind", lapply(adr, function(x) geocodeSL(x, myToken)))
-
+# geocode with US Street Address
+do.call("rbind", lapply(adr, function(x) geocodeSL(x, myToken, geocoder = "USA_Str")))
+```
 
 ### Multi Line Batch
+
 `geocodeML_batch` takes the address in separate fields and sends them all at once in a POST request.
+
+**Arguments:**
+
+id - ID variable to identify records, must be numeric and should be unique
+street, city, state, zip - multiple addresses as vectors, separated into: Street, City, State, Zip,. It can take a maximum of 1000 adresses. If more, it returns an error.  
+token - token  
+geocoder - which geocoding service to use: USA_Comp (USA Composite) or USA_Str (USA StreetAddress), default is USA_Comp  
+
+
+**The function returns** 
+
+A data frame with the following fields:
+
+ID -          Result ID can be used to join the output fields in the response to the attributes 
+               in the original address table.  
+lon, lat -    The primary x/y coordinates of the address returned by the geocoding service in WGS84  
+score -       The accuracy of the address match between 0 and 100.  
+status -      Whether a batch geocode request results in a match (M), tie (T), or unmatch (U)  
+matchAddr -   Complete address returned for the geocode request.  
+side -        The side of the street where an address resides relative to the direction 
+               of feature digitization  
+addressType - The match level for a geocode request. "PointAddress" is typically the 
+               most spatially accurate match level. "StreetAddress" differs from PointAddress 
+               because the house number is interpolated from a range of numbers. "StreetName" is similar,
+               but without the house number.  
+
 
 To use it do this.
 
-    # make up a data frame with some addresses:
-    adr_df <- data.frame(
-               ID = 1:3,
-               street = c('450 Serra Mall', '1600 Amphitheatre Pkwy', '1355 Market Street Suite 900'), 
-               city = c('Stanford', 'Mountain View', 'San Francisco'), 
-               state = 'CA', 
-               zip = c('94305', '94043', '94103'))
+``` R
+# make up a data frame with some addresses:
+adr_df <- data.frame(
+  ID = 1:3,
+  street = c('450 Serra Mall', '1600 Amphitheatre Pkwy', '1355 Market Street Suite 900'), 
+  city = c('Stanford', 'Mountain View', 'San Francisco'), 
+  state = 'CA', 
+  zip = c('94305', '94043', '94103'))
 
-    # source the R code
-    source("https://raw.githubusercontent.com/cengel/ArcGIS_geocoding/master/SUL_gcFunctions.R")
-
-    # set your token
-    myToken <- "YOUR TOKEN HERE"
+# set your token
+myToken <- "YOUR TOKEN HERE"
     
-    # geocode with
-    adr_gc <- geocodeML_batch(adr_df$ID, adr_df$street, adr_df$city, adr_df$state, adr_df$zip, myToken)
-    
-    # join back with original data
-    merge(adr_df, adr_gc, by = "ID", all.x = T)
+# geocode with US Composite
+adr_gc_comp <- geocodeML_batch(adr_df$ID, adr_df$street, adr_df$city, adr_df$state, adr_df$zip, myToken)
+# join back with original data
+merge(adr_df, adr_gc_comp, by = "ID", all.x = T)
 
+# geocode with US Street Address
+adr_gc_street <- geocodeML_batch(adr_df$ID, adr_df$street, adr_df$city, adr_df$state, adr_df$zip, myToken, geocoder = "USA_Str")
+# join back with original data
+merge(adr_df, adr_gc_street, by = "ID", all.x = T)
+```
 
 ## References
 
--- SUL_gcFunctions.R
+++ SUL_gcFunctions.R
@@ -4,12 +4,12 @@
 # The function takes:
 # - one address at a time as one string (SingleLine)
 # - token
+# - which geocoding service to use: USA_Comp (USA Composite) or USA_Str (USA StreetAddress) (default is USA_Comp)
 # - allow to return Postal codes if a full street address match cannot be found (default is TRUE)
 #
 # The function returns:
 # lon, lat -    The primary x/y coordinates of the address returned by the geocoding service in WGS84 
 # score -       The accuracy of the address match between 0 and 100.
-# locName -     The component locator used to return a particular match result
 # status -      Whether a batch geocode request results in a match (M), tie (T), or unmatch (U)
 # matchAddr -   Complete address returned for the geocode request.
 # side -        The side of the street where an address resides relative to the direction 
@@ -19,15 +19,23 @@
 #               because the house number is interpolated from a range of numbers. "StreetName" is similar,
 #               but without the house number.
 
-geocodeSL <- function (address, token, postal = TRUE){
+geocodeSL <- function (address, token, geocoder = "USA_Comp", postal = TRUE){
   require(httr)
   
-  # Stanford geolocator
-  gserver <- "http://locator.stanford.edu/arcgis/rest/services/geocode/USA_Composite/GeocodeServer/geocodeAddresses"
+  if (geocoder == "USA_Str"){
+    # Stanford geolocator
+    gserver <- "http://locator.stanford.edu/arcgis/rest/services/geocode/USA_StreetAddress/GeocodeServer/geocodeAddresses"
+    # template for Single Line format
+    pref <- "{'records':[{'attributes':{'OBJECTID':1,'Single Line Input':'"
+  }
+  else if (geocoder == "USA_Comp") {
+    gserver <- "http://locator.stanford.edu/arcgis/rest/services/geocode/USA_Composite/GeocodeServer/geocodeAddresses"
+    pref <- "{'records':[{'attributes':{'OBJECTID':1,'SingleLine':'"
+  }
+  else{
+    stop("please provide a valid geocoder")
+  }
   
-
-  # template for SingleLine format
-  pref <- "{'records':[{'attributes':{'OBJECTID':1,'SingleLine':'"
   suff <- "'}}]}"
   
   # url
@@ -41,7 +49,7 @@ geocodeSL <- function (address, token, postal = TRUE){
   resdf <- with(res$locations[[1]], {data.frame(lon = as.numeric(location$x),
                                                 lat = as.numeric(location$y),
                                                 score = score, 
-                                                locName = attributes$Loc_name,
+                                                #locName = attributes$Loc_name,
                                                 status = attributes$Status,
                                                 matchAddr = attributes$Match_addr,
                                                 side = attributes$Side,
@@ -56,8 +64,10 @@ geocodeSL <- function (address, token, postal = TRUE){
 # - ID variable to identify records, must be numeric and should be unique
 # - multiple addresses as vectors, separated into: Street, City, State, Zip
 # - token
+# - which geocoding service to use: USA_Comp (USA Composite) or USA_Str (USA StreetAddress) (default is USA_Comp)
+
 #
-# It can take a maximum of 1000 addresses. If more, it returns an error.
+# It can take a maximum of 1000 adresses. If more, it returns an error.
 #
 # The function returns a data frame with the following fields:
 # ID -          Result ID can be used to join the output fields in the response to the attributes 
@@ -73,8 +83,8 @@ geocodeSL <- function (address, token, postal = TRUE){
 #               most spatially accurate match level. "StreetAddress" differs from PointAddress 
 #               because the house number is interpolated from a range of numbers. "StreetName" is similar,
 #               but without the house number.
-
-geocodeML_batch <- function(id, street, city, state, zip, token){
+ 
+geocodeML_batch <- function(id, street, city, state, zip, token, geocoder = "USA_Comp"){
   require(httr)
   require(rjson)
   
@@ -83,29 +93,40 @@ geocodeML_batch <- function(id, street, city, state, zip, token){
     print(paste("length is: ", length(id)))
     stop("Can only process up to 1000 addresses at a time.")}
   
-  # check if id is numeric
-  if (!is.numeric(id)) {
-    stop("id variable needs to be numeric.")
+  # check if id is numeric, either a real number or a string number
+  if(!all(grepl("^[0-9]{1,}$", id))){ # HT: https://stackoverflow.com/a/48954452/2630957
+  #if (!is.numeric(id)) {
+    stop("id variable needs to be a number")
   }
   
   # make data frame
-  adr_df <- data.frame(OBJECTID = id, 
+  adr_df <- data.frame(OBJECTID = id,  # we need the id to be called OBJECTID
                        Street = street,
                        City = city,
                        State = state,
                        Zip = zip)
-  
+
   # Set missing ZIP codes to empty strings
-  adr_df$Zip <- ifelse(is.na(adr_df$Zip), '', adr_df$Zip)
+  adr_df$Zip <- ifelse(is.na(adr_df$Zip), '', as.character(adr_df$Zip))
   
   # make json
   tmp_list <- apply(adr_df, 1, function(i) list(attributes = as.list(i)))
-  # need to coerce ID back to numeric
-  tmp_list <- lapply(tmp_list, function(i) { i$attributes$OBJECTID <- as.numeric(i$attributes$OBJECTID); i })
+  # need to coerce OBJECTID to numeric
+  tmp_list <- lapply(tmp_list, function(i) { 
+    i$attributes$OBJECTID <- as.numeric(i$attributes$OBJECTID); 
+    i})
+
   adr_json <- toJSON(list(records = tmp_list))
   
-  gserver <- "http://locator.stanford.edu/arcgis/rest/services/geocode/USA_Composite/GeocodeServer/geocodeAddresses"
-  
+  if(geocoder == "USA_Comp"){
+    gserver <- "http://locator.stanford.edu/arcgis/rest/services/geocode/USA_Composite/GeocodeServer/geocodeAddresses"
+    }
+  else if(geocoder == "USA_Str"){
+    gserver <- "http://locator.stanford.edu/arcgis/rest/services/geocode/USA_StreetAddress/GeocodeServer/geocodeAddresses"
+  }
+  else{
+    stop(paste(geocoder, "please provide a valid geocoder"))
+  }
   
   # submit
   req <- POST(
@@ -116,18 +137,20 @@ geocodeML_batch <- function(id, street, city, state, zip, token){
   
   # process and parse
   res <- content(req, "parsed", "application/json")
+  
   resdfr <- data.frame()
   for (i in seq_len(length(res$locations))){
     d <- with(res$locations[[i]], {data.frame(ID = attributes$ResultID,
                                               lon = as.numeric(location$x),
                                               lat = as.numeric(location$y),
                                               score = score, 
-                                              locName = attributes$Loc_name,
+                                              #locName = attributes$Loc_name,
                                               status = attributes$Status,
                                               matchAddr = attributes$Match_addr,
                                               side = attributes$Side,
                                               addressType = attributes$Addr_type)})
     resdfr <- rbind(resdfr, d)
   }
+
   return(resdfr)
 }
